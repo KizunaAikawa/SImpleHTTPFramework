9d79cf066ff7f4841958584beacee5c028fb5877
TheVoid <593767573@qq.com>
2019-07-19 17:33:41 +0800

    增加了用于测试的语句

--- src/main/java/cn/voidnet/SimpleHTTPFramework/Main.java
+++ src/main/java/cn/voidnet/SimpleHTTPFramework/Main.java
@@ -3,5 +3,6 @@ package cn.voidnet.SimpleHTTPFramework;
 public class Main {
     public static void main(String[] args) throws Exception {
         var server = new HTTPServer(8080, new SimpleServletImpl());
+        server.start();
     }
 }
