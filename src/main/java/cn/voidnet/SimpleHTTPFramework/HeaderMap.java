50d9f25ef36825815aa49589cc9c905066f2c285
TheVoid <593767573@qq.com>
2019-07-19 17:33:25 +0800

    删除了两个没用的类

--- src/main/java/cn/voidnet/SimpleHTTPFramework/HeaderMap.java
+++
@@ -1,9 +0,0 @@
-package cn.voidnet.SimpleHTTPFramework;
-
-import java.util.HashMap;
-
-public class HeaderMap extends HashMap<String, String> {
-    public HeaderMap(String headerStr) {
-        //TODO:写Header字符串转HeaderMap的转换函数
-    }
-}
--- src/main/java/cn/voidnet/SimpleHTTPFramework/ParameterMap.java
+++
@@ -1,9 +0,0 @@
-package cn.voidnet.SimpleHTTPFramework;
-
-import java.util.HashMap;
-
-public class ParameterMap extends HashMap<String, String> {
-    public ParameterMap(String headerStr) {
-        //TODO:写Header字符串转ParameterMap的转换函数
-    }
-}
